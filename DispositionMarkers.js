5f28aa5ff4c56dfcad10f11cf23d5e6f300fabb4
QueasyBun <99047447+QueasyBun@users.noreply.github.com>
2022-11-24 21:04:20 +0100

    Update DispositionMarkers.js

--- DispositionMarkers.js
+++ DispositionMarkers.js
@@ -22,46 +22,45 @@ let usesTagger = true;
 
 //Setup script, create the parameters as global settings so they're accessible later and don't have to be redefined every time.
 Hooks.once("setup", () => {
-   console.info("World Script | Disposition markers | Registering disposition marker worldscript");
+    console.info("World Script | Disposition markers | Registering disposition marker worldscript");
    
-   //Setup the first set of parameters
-   globalThis.DispositionParamsFriendly =
-   [{
-      filterId: "dispositionMarker",
-      filterType: "glow",
-      outerStrength: 4,
-      color: 0x14a647,
-      padding: 40
-   }];
+    //Setup the first set of parameters
+    globalThis.DispositionParamsFriendly =
+    [{
+        filterId: "dispositionMarker",
+        filterType: "glow",
+        outerStrength: 4,
+        color: 0x14a647,
+        padding: 40
+    }];
    
-   //Base the other two off of the first one.
-   globalThis.DispositionParamsNeutral = [{...globalThis.DispositionParamsFriendly[0], color : 0x2596be}];
-   globalThis.DispositionParamsHostile = [{...globalThis.DispositionParamsFriendly[0], color : 0xfc0703}];
+    //Base the other two off of the first one.
+    globalThis.DispositionParamsNeutral = [{...globalThis.DispositionParamsFriendly[0], color : 0x2596be}];
+    globalThis.DispositionParamsHostile = [{...globalThis.DispositionParamsFriendly[0], color : 0xfc0703}];
 });
 
 //Trigger every time you switch maps to check for tokens that should have a disposition marker.
-Hooks.on("canvasReady", () => {
+Hooks.on("canvasReady", () => {  
     let allTokens;
     
     //If tagger is used, grab all tokens that are tagged as Social. If not, just grab all tokens.
     if(usesTagger == true)
-        allTokens = Tagger.getByTag("Social");
+        allTokens = canvas.tokens.placeables.filter(token => {token.document.flags?.tagger?.tags?.includes("Social")}); //map(token => {if(token.document.flags?.tagger?.tags?.includes("Social")) return token; } )
     else
-        allTokens = canvas.tokens.placeables.map(token => token.document);
+        allTokens = canvas.tokens.placeables;
     
     //Set a disposition marker for all relevant tokens.
     allTokens.forEach(token => {
-        setDispositionMarker(token._object, token.disposition);
+        setDispositionMarker(token, token.disposition);
     });
 });
 
 //Trigger every time a token is updated.
-console.log("World Script | Disposition markers | Disposition Marker update hook index is " + Hooks.on("updateToken", async function(doc, change) {
-   
+console.log("World Script | Disposition markers | Disposition Marker update hook index is " + Hooks.on("updateToken", async function(doc, change) {  
     //If the change is in the tokenmagic flags, do nothing (Adding and deleting filters later will trigger another instance of this event, so this check prevents a loop).
     if(change.flags?.tokenmagic != undefined)
         return;
-     
+        
     //Clear any disposition markers on social tokens. Doing this before the Social check so that removing the tag also removes the marker.
     //Keep in mind that the flag is not cleared until all events are resolved, so this check still returns true even if this event removes the tag.
     if(!doc.flags?.tagger?.tags?.includes("Social") && usesTagger)
@@ -77,10 +76,10 @@ console.log("World Script | Disposition markers | Disposition Marker update hook
     //If the change does not include a change to disposition, and this change does not add the social tag, do nothing.
     //If the change does add the social tag, grab the token's current distribution.
     let disposition = changeHasSocialFlag ? doc.disposition : change.disposition;
-    
+        
     if(disposition == undefined)
         return;
-
+    
     //If it made it this far, set the disposition marker.
     setDispositionMarker(doc._object,disposition);
 }));
